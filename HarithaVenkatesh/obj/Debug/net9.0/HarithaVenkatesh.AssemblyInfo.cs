41f57960f58a00beafcc4d30fa47ead628ea6c00
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2025-07-15 22:05:26 +0530

    Removing Obj and Bin files

--- HarithaVenkatesh/obj/Debug/net9.0/HarithaVenkatesh.AssemblyInfo.cs
+++ HarithaVenkatesh/obj/Debug/net9.0/HarithaVenkatesh.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarithaVenkatesh")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7f420a6d831e04e3923022492e081488a5ad6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarithaVenkatesh")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarithaVenkatesh")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
